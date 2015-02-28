20a5d3f5baf421e276703a6130849b36889b0665
Claude LE BRIS <claude.lebris@gmail.com>
2015-02-28 23:51:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Doctrine DBAL service provider
+
+A silex provider for Doctine DBAL using puzzle/configuration (https://github.com/Niktux/puzzle-configuration)
+
+## Installation
+
+```
+{
+    "require": {
+        "silex-spear/dbal-provider": "~1.0"
+    }
+}
+```
+
+or 
+
+```bash
+composer require silex-spear/dbal-provider
+```
+
+#### What is the `config/db.yml-dist.example` file ?
+
+It's a configuration example file for the configuration file hydrator karma (https://github.com/Niktux/karma)
---
+++ src/DBAL.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Spear\Silex\Provider;
+
+use Silex\ServiceProviderInterface;
+use Puzzle\Configuration;
+use Silex\Application;
+use Silex\Provider\DoctrineServiceProvider;
+
+class DBAL implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $this->validatePuzzleConfiguration($app);
+
+        $app->register(new DoctrineServiceProvider(), array(
+            'db.options' => array(
+                'driver'   => 'pdo_mysql',
+                'dbname'   => $app['configuration']->readRequired('db/database'),
+                'host'     => $app['configuration']->readRequired('db/host'),
+                'user'     => $app['configuration']->readRequired('db/user'),
+                'password' => $app['configuration']->readRequired('db/password'),
+                'port'     => $app['configuration']->read('db/port', 3306),
+                'charset'  => $app['configuration']->read('db/charset', 'utf8'),
+            )
+        ));
+    }
+
+    public function boot(Application $app)
+    {
+        ;
+    }
+
+    private function validatePuzzleConfiguration(Application $app)
+    {
+        if(! isset($app['configuration']) || ! $app['configuration'] instanceof Configuration)
+        {
+            throw new \LogicException('AsseticProvider requires an instance of puzzle/configuration for the key configuration to be defined.');
+        }
+    }
+}
