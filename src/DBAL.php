c551d413f78b4170972a614895eda61b9df1bb3e
Niktux <nico.ln@gmail.com>
2015-03-01 02:11:31 +0100

    feat: allow handling many database connections

--- src/DBAL.php
+++ src/DBAL.php
@@ -6,29 +6,56 @@ use Silex\ServiceProviderInterface;
 use Puzzle\Configuration;
 use Silex\Application;
 use Silex\Provider\DoctrineServiceProvider;
+use Puzzle\PrefixedConfiguration;
 
 class DBAL implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
         $this->validatePuzzleConfiguration($app);
+        $this->registerDatabases($app);
+    }
+
+    private function registerDatabases(Application $app)
+    {
+        $options = array();
+        $configuration = $app['configuration'];
+
+        $databases = array_keys($configuration->readRequired('db'));
+        foreach($databases as $database)
+        {
+            $options[$database] = $this->registerDatabase($app, $configuration, $database);
+        }
 
         $app->register(new DoctrineServiceProvider(), array(
-            'db.options' => array(
-                'driver'   => 'pdo_mysql',
-                'dbname'   => $app['configuration']->readRequired('db/database'),
-                'host'     => $app['configuration']->readRequired('db/host'),
-                'user'     => $app['configuration']->readRequired('db/user'),
-                'password' => $app['configuration']->readRequired('db/password'),
-                'port'     => $app['configuration']->read('db/port', 3306),
-                'charset'  => $app['configuration']->read('db/charset', 'utf8'),
-            )
+            'dbs.options' => $options
         ));
+    }
+
+    private function registerDatabase(Application $app, Configuration $configuration, $database)
+    {
+        $configuration = new PrefixedConfiguration($configuration, "db/$database");
+
+        $options = array(
+            'driver'   => 'pdo_mysql',
+            'dbname'   => $configuration->readRequired('database'),
+            'host'     => $configuration->readRequired('host'),
+            'user'     => $configuration->readRequired('user'),
+            'password' => $configuration->readRequired('password'),
+            'port'     => $configuration->read('port', 3306),
+            'charset'  => $configuration->read('charset', 'utf8'),
+        );
+
+        // Declare helper
+        $app["db.$database"] = $app->share(function () use ($app, $database) {
+            return $app['dbs'][$database];
+        });
+
+        return $options;
     }
 
     public function boot(Application $app)
     {
-        ;
     }
 
     private function validatePuzzleConfiguration(Application $app)
